5681374fdc18c14b2f8a4a9194c3123a5e68b3e9
Akbarjon Ismaylov <akbarjonismaylov5@gmail.com>
2025-12-04 15:12:46 +0500

    cods

--- Lesson2/Lesson2/Lesson2/Program.cs
+++ Lesson2/Lesson2/Lesson2/Program.cs
@@ -1,10 +1,66 @@
-﻿namespace Lesson2
+﻿using System;
+namespace Lesson2
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("This is a super cool program by Akbarjon Ismaylov!");
+
+            /*Console.Write("son kiriting: ");
+            int number = int.Parse(Console.ReadLine());
+            int sum = number * 10;
+            int subtract = number - 10;
+
+            string result = $"Result: (*) {sum}, (-) {subtract}";
+
+            Console.WriteLine(result);*/
+
+            /*  ---------*/
+
+            /*Console.Write("son kiriting: ");
+            int number = int.Parse(Console.ReadLine());
+            int sum = number * number * number;
+
+            string result = $"Result = {sum}";
+
+            Console.WriteLine(result);*/
+
+            /*  ------------*/
+
+
+            /*Console.Write("son kiriting: ");
+            int number = int.Parse(Console.ReadLine());
+            int sum = number * number;
+
+            string result = $"Result = {sum}";
+
+            Console.WriteLine(result);*/
+
+            /*------------*/
+
+            /* Console.Write("son kiriting: ");
+             double number = int.Parse(Console.ReadLine());
+
+             double sum = Math.Pow(number, 5);
+
+             string result = $"Result = {sum}";
+
+             Console.WriteLine(result);*/
+
+            /*------------*/
+
+            Console.Write("birinchi sonni kiriting: ");
+
+            double NumberOne = double.Parse(Console.ReadLine());
+
+            Console.Write("ikkinchi sonni kiriting: ");
+
+            double NumberTwo = double.Parse(Console.ReadLine());
+
+            double sum = NumberOne + NumberTwo;
+
+            Console.WriteLine(sum);
+
         }
     }
 }
