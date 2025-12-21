979cf5a36d18574c43b33d5816b3fb85bd6626f6
Akbarjon Ismaylov <akbarjonismaylov5@gmail.com>
2025-12-21 16:49:08 +0500

    class codes

---
+++ LessonLeedCode/LessonLeedCode/LessonClass.cs
@@ -0,0 +1,17 @@
+﻿namespace LessonLeedCode
+{
+
+    public class User
+    {
+        public string name {get; set; }
+
+        public int age { get; set; }
+
+        public string email { get; set; }
+
+        public string id { get; set; }
+
+        public string phone { get; set; }
+    }
+
+}
---
+++ LessonLeedCode/LessonLeedCode/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace LessonLeedCode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Name > ");
+            string Name = Console.ReadLine();
+
+            Console.Write("Age > ");
+            int Age = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter your email > ");
+            string Email = Console.ReadLine();
+
+            Console.Write("Enter your ID > ");
+            string Id = Console.ReadLine();
+
+            Console.Write("Enter your phone number > ");
+            string Phone = Console.ReadLine();
+
+            User user = new User();
+
+            user.name = Name;
+            Console.WriteLine($"Name > {user.name}");
+
+            user.age = Age;
+            Console.WriteLine($"Age > {user.age}");
+
+            user.email = Email;
+            Console.WriteLine($"Email > {user.email}");
+
+            user.id = Id;
+            Console.WriteLine($"ID > {user.id}");
+
+            user.phone = Phone;
+            Console.WriteLine($"Phone number > {user.id}");
+
+        }
+    }
+}
+
+
