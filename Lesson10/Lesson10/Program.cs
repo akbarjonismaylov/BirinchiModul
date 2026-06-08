6fb51a9379af9091fc1ffcbc26b300602c765121
Akbarjon Ismaylov <akbarjonismaylov5@gmail.com>
2026-06-08 16:41:05 +0500

    new lesson

---
+++ Lesson10/Lesson10/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Lesson10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            student student = new student();
+            student.Name = "Akbarjon";
+            student.Surname = "Ismaylov";
+            student.Age = 21;
+            student.Group = "515_22";
+            student.University = "TUIT";
+
+            Console.WriteLine($"Name: {student.Name}");
+            Console.WriteLine($"Surname: {student.Surname}");
+            Console.WriteLine($"Age: {student.Age}");
+            Console.WriteLine($"Group: {student.Group}");
+            Console.WriteLine($"University: {student.University}");
+
+            Console.WriteLine();
+
+            student student2 = new student();
+            student2.Name = "Shaxzod";
+            student2.Surname = "Ismaylov";
+            student2.Age = 21;
+            student2.Group = "515_22";
+            student2.University = "TUIT";
+
+            Console.WriteLine($"Name: {student2.Name}");
+            Console.WriteLine($"Surname: {student2.Surname}");
+            Console.WriteLine($"Age: {student2.Age}");
+            Console.WriteLine($"Group: {student2.Group}");
+            Console.WriteLine($"University: {student2.University}");
+
+
+        }
+    }
+}
+
+
---
+++ Lesson10/Lesson10/student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson10;
+
+public class student
+{
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public int Age { get; set; }
+    public string Group { get; set; }
+    public string University { get; set; }
+}
