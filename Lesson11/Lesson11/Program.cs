8e920513c4f772360f61f34df6db388e81327026
Akbarjon Ismaylov <akbarjonismaylov5@gmail.com>
2026-06-12 18:36:02 +0500

    update codes

--- Lesson11/Lesson11/Program.cs
+++ Lesson11/Lesson11/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading.Channels;
 
 namespace Lesson11
 {
@@ -6,34 +7,53 @@ namespace Lesson11
     {
         static void Main(string[] args)
         {
-            var school = CreatSchool();
-            PrintSchoolInfo(school);
-            Console.WriteLine(CreatSchool());
+            var StudentPassword = "Ismaylov";
+
+            Console.Write("Password > ");
+            var inputPassword = Console.ReadLine();
+
+            if (StudentPassword == inputPassword)
+            {
+                Console.WriteLine("CREATED!!!");
+                Student student = CreateStudent();
+                PrintStudent(student);
+                Display(student);
+            }
+            else
+            {
+                Console.WriteLine("Wrong password!!!");
+            }
+        }
+        static Student CreateStudent()
+        {
+            return new Student();
         }
 
-        static School CreatSchool()
+        static void PrintStudent(Student student)
         {
-            School school = new School();
-            return school;
+            Console.Write($"Name: ");
+            student.Name = Console.ReadLine();
+            Console.Write($"Age: ");
+            student.Age = int.Parse(Console.ReadLine());
+            Console.Write($"School Name: ");
+            student.SchoolName = Console.ReadLine();
+            Console.Write($"Group: ");
+            student.Group = Console.ReadLine();
+            Console.Write($"Course: ");
+            student.Course = Console.ReadLine();
+
+            Console.WriteLine();
         }
 
-        static void PrintSchoolInfo(School school)
+
+        static void Display(Student student)
         {
-            Console.Write("School name > ");
-            school.NameSchool = Console.ReadLine();
-
-            Console.Write("School number > ");
-            school.NUmberSchool = int.Parse(Console.ReadLine());
-
-            Console.Write("School address > ");
-            school.AdressSchool = Console.ReadLine();
-
-            Console.Write("Student capacity > ");
-            school.Capacity = int.Parse(Console.ReadLine());
-
-            Console.Write("Current student count > ");
-            school.CurrentStudentCount = int.Parse(Console.ReadLine());
-
+            
+            Console.WriteLine($"Name: {student.Name}");
+            Console.WriteLine($"Age: {student.Age}");
+            Console.WriteLine($"School Name: {student.SchoolName}");
+            Console.WriteLine($"Group: {student.Group}");
+            Console.WriteLine($"Course: {student.Course}");
         }
     }
 }
---
+++ Lesson11/Lesson11/Student.cs
@@ -0,0 +1,10 @@
+﻿namespace Lesson11;
+
+public class Student
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public string SchoolName { get; set; }
+    public string Group { get; set; }
+    public string Course { get; set; }
+}
